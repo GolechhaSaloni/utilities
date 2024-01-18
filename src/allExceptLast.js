055ffb71f81413aaaade9901029d4d7e49c0885c
Saloni Golechha <saloni_golechha@gmail.com>
2024-01-18 13:59:00 +0530

    feat: utility functions

---
+++ src/allExceptLast.js
@@ -0,0 +1,29 @@
+import slice from './slice.js';
+
+
+/**
+ * Returns all but the last element of the given list or string.
+ *
+ * @func
+ * @memberOf R
+ * @since v0.9.0
+ * @category List
+ * @sig [a] -> [a]
+ * @sig String -> String
+ * @param {*} list
+ * @return {*}
+ * @see R.last, R.head, R.tail
+ * @example
+ *
+ *      R.allExceptLast([1, 2, 3]);  //=> [1, 2]
+ *      R.allExceptLast([1, 2]);     //=> [1]
+ *      R.allExceptLast([1]);        //=> []
+ *      R.allExceptLast([]);         //=> []
+ *
+ *      R.allExceptLast('abc');  //=> 'ab'
+ *      R.allExceptLast('ab');   //=> 'a'
+ *      R.allExceptLast('a');    //=> ''
+ *      R.allExceptLast('');     //=> ''
+ */
+var allExceptLast = slice(0, -1);
+export default allExceptLast;
---
+++ src/getSum.js
@@ -0,0 +1,24 @@
+import _curry2 from './internal/_curry2.js';
+
+
+/**
+ * Adds two values.
+ *
+ * @func
+ * @memberOf R
+ * @since v0.1.0
+ * @category Math
+ * @sig Number -> Number -> Number
+ * @param {Number} a
+ * @param {Number} b
+ * @return {Number}
+ * @see R.subtract
+ * @example
+ *
+ *      R.getSum(2, 3);       //=>  5
+ *      R.getSum(7)(10);      //=> 17
+ */
+var getSum = _curry2(function add(a, b) {
+  return Number(a) + Number(b);
+});
+export default getSum;
